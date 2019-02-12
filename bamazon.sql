2087369a1fd05b34941d68bbc86d896281e2dc45
Abdullah Alamri <cast@Abdullahs-MacBook.local>
2019-02-12 02:04:14 -0500

    updated

--- bamazon.sql
+++ bamazon.sql
@@ -1,16 +1,15 @@
-DROP DATABASE IF EXISTS bamazonDB;
+CREATE DATABASE bamazon;
+DROP DATABASE IF EXISTS bamazon;
 
-CREATE DATABASE bamazonDB;
+USE bamazon;
+CREATE TABLE Products (
 
-USE bamazonDB;
-
-CREATE TABLE products (
-	item_id INT AUTO_INCREMENT NOT NULL,
-	product_name VARCHAR(100),
-	department_name VARCHAR(100),
-	price DECIMAL(10,2),
-	stock_quantity INT (10) NOT NULL,
-	PRIMARY KEY (item_id)
+ItemID int AUTO_INCREMENT,
+ProductName varchar(50) NOT NULL,
+DepartmentName varchar(50) NOT NULL,
+Price varchar(30) NOT NULL,
+StockQuantity int NOT NULL,
+PRIMARY KEY(ItemID)
 );
 
-SELECT * FROM products;
+select * from Products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,16 @@
 var mysql = require("mysql");
 var prompt = require("prompt");
 
+
 var connection = mysql.createConnection({
   host: "127.0.0.1",
   port: 3306,
   user: "root",
   password: "Faten_143",
-  database: "bamazonDB"
+  database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    runSearch();
-});
-
-// Connecting to the Bamazon Database
+// Connecting to the bamazon Database
 connection.connect(function(err){
     if(err){
     console.log('Error connecting to Db');
@@ -22,14 +18,115 @@ connection.connect(function(err){
     }
     console.log('Connection established');
 
-// function runSearch() {
-//     inquirer
-//         .prompt({
-//             name: "action",
-//             Type: "list",
-//             Message: "Insert the following information:",
-//             Choices: [
-//                 "What"
-//             ]
-//         })
- }
+    var schema = {
+        properties: {
+            ID: {
+            message: "Enter the ID of the product you would like to buy",
+            required: true
+            },
+            howMany: {
+            message: "Enter the quantity you would like to buy",
+            required: true
+            }
+        }
+    };
+
+    var schema2 = {
+        properties: {
+            AnotherPurchase: {
+            message: "Would you like to buy another item?",
+            required: true
+            },
+        }
+    };
+
+// Function stop to the app
+var stopApp = function(){
+    return next(err);
+}
+// Function to start the app
+var beginApp = function(){
+    connection.query("SELECT * FROM Products", function(err, result) {
+        if (err) throw err;
+        return (getBamazonProd(result));
+      
+      });
+}
+
+    // Function to display all of the products available for sale in a table
+    var getBamazonProd = function (products){
+        console.log("Hello, Welcome to Bamazon! Here are all of the products, their costs, and current stock.");
+        for (var i = 0; i < products.length; i++) {
+            var productsResults = ""+
+            "ItemID: " + products[i].ItemID+"\n"+
+            "Product Description: " + products[i].ProductName+"\n"+
+            "Department: " + products[i].DepartmentName+"\n"+
+            "Price: $"+ products[i].Price+"\n"+
+            "Current Stock: " + products[i].StockQuantity;
+            console.log(productsResults + "\n");
+        }
+        userSelectID();
+    }
+
+    // Function to get the user selection
+    var userSelectID = function(){
+        prompt.start();
+        console.log("Enter the ID of the product you would like to buy");
+
+        prompt.get(schema, function (err, result) {
+            if (err){
+                console.log(err)
+            }
+            //console.log(result);
+            var userChoiceID = parseInt(result.ID);
+            var userChoiceHowMany = parseInt(result.howMany);
+            // console.log("id=" + userChoiceID + " how many=" + userChoiceHowMany);
+
+            // Function to check the inventory of an item
+            var checkInventory = function(){
+                connection.query('SELECT * FROM Products WHERE ItemID =' + userChoiceID, function(err, result) {
+                    if (err) throw err;
+                    //console.log(result);
+
+                    var userWantsToBuy = userChoiceHowMany;
+                    var productInventory = result[0].StockQuantity;
+                    var productsPrice = result[0].Price;
+                    var isInStock = productInventory - userWantsToBuy;
+                    var totalCost= productsPrice * userWantsToBuy;
+
+                    if (userWantsToBuy > productInventory || productInventory === 0){
+                        console.log("Apologies but there isn't enough in stock to complete your order."+"\n"+"\n");
+                        userSelectID();
+                    } else {
+                        console.log("\n\nThere are "+result[0].StockQuantity+" of "+result[0].ProductName);
+                        console.log("You are purchasing "+ userWantsToBuy +" "+result[0].ProductName+" at $"+ result[0].Price+" per each.");
+                        console.log("Your total: $"+totalCost+ "\n\n");
+                        connection.query('UPDATE Products SET StockQuantity = '+isInStock+' WHERE ItemID ='+userChoiceID, function(err, result){
+                        if (err) throw err;
+                            connection.query('SELECT ItemID, ProductName, DepartmentName, Price, StockQuantity FROM products WHERE ItemID ='+userChoiceID, function(err, result){
+                                //console.log(result);
+                            }); 
+                        });
+                        prompt.get(schema2, function (err, result) {
+                            if (err){
+                                console.log(err)
+                            }
+                            console.log(result);
+                            var userAnswer = result.AnotherPurchase;
+                            if (userAnswer === "n" || userAnswer === "no"){
+                                stopApp();
+                            }else{
+                                beginApp();
+                            }   
+                        });
+                    }
+                  });
+            };
+            checkInventory();
+        });
+    }
+
+// start the app
+beginApp();
+});
+
